94aab04545709012ca83c93a807d3215121bee19
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-10-28 02:22:27 -0300

    Add handler for unauthorized and forbidden exceptions

--- src/main/java/com/security/jwt/exceptions/RestExceptionHandler.java
+++ src/main/java/com/security/jwt/exceptions/RestExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -15,6 +16,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 	
+	/**
+	 * Handles invalid arguments exception.
+	 * 
+	 */
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -32,6 +37,41 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 		response.addError(error);
 		return ResponseEntity.unprocessableEntity().body(response);
 		
+	}
+	
+	/**
+	 * Handles unauthorized exception. This exception indicates that the request requires
+	 * authentication or the authentication process has invalid parameters.
+	 * 
+	 * @param untException
+	 * @return
+	 */
+	@ExceptionHandler(UnauthorizedUserException.class)
+	public ResponseEntity<Response<?>> handleUnauthorizedUserException(UnauthorizedUserException untException){
+		Response<String> response = new Response<String>();
+		ErrorDetail errorDetail = new ErrorDetail.Builder()
+				.message(untException.getMessage())
+				.build();
+		response.addError(errorDetail);
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+	}
+	
+	/**
+	 * Handles forbidden exception. The forbidden exception occurs when the server understood 
+	 * the request but refused to fulfill it. Example: the user has insufficient credentials
+	 * to access an specific URL. 
+	 * 
+	 * @param fbdException
+	 * @return
+	 */
+	@ExceptionHandler(ForbiddenUserException.class)
+	public ResponseEntity<Response<?>> handleForbiddenUserException(ForbiddenUserException fbdException){
+		Response<String> response = new Response<String>();
+		ErrorDetail errorDetail = new ErrorDetail.Builder()
+				.message(fbdException.getMessage())
+				.build();
+		response.addError(errorDetail);
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
 	}
 	
 }
